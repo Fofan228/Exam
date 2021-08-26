100d44f7bd9cb95ab068de30a7a8e88b4d97d3e4
Vova <vovka.322@mail.ru>
2021-08-27 00:10:07 +0600

    CreateJsonProject

---
+++ JS/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace JS
+{
+    using Newtonsoft.Json;
+    using System;
+    using System.IO;
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Order order1 = GetOrder(10);
+            SaveText(order1);
+            var deOrder = DeserelezadeText();
+            Console.WriteLine(deOrder.customer.Name);
+            Console.WriteLine(deOrder.product.Name);
+            Console.WriteLine(deOrder.FullPrice);
+
+        }
+        private static Order GetOrder(int number)
+        {
+            var order = new Order();
+            order.customer = new Customer("Петя", Sexs.Male, 7);
+            order.Number = number;
+            order.product = new Product("Алёнка", Categorys.Chocolate, 42.5f);
+            return order;
+        }
+        private static void SaveText(Order order)
+        {
+            string path = Environment.CurrentDirectory + "\\JS.json";
+            File.Create(path).Dispose();
+            string text = JsonConvert.SerializeObject(order);
+            File.WriteAllText(path, text);
+        }
+        private static Order DeserelezadeText()
+        {
+            string path = Environment.CurrentDirectory + "\\JS.json";
+            if (File.Exists(path))
+            {
+                string json = File.ReadAllText(path);
+                var orderExit = JsonConvert.DeserializeObject<Order>(json);
+                return orderExit;
+            }
+            else
+            {
+                return new Order();
+            }
+        }
+    }
+}
+// Данные получают из файла: товар (название, категория, цена за единицу,),кол-во товара, покупатель (имя, пол, размер члена)
+//составить таблицу в которой будет: покупатель, товар, кол-во штук, общая сумма (от большего к меньшему)
---
+++ JS/customer.cs
@@ -0,0 +1,59 @@
+﻿using Newtonsoft.Json;
+
+namespace JS
+{
+    enum Categorys
+    {
+        Chocolate, Soda, Marmalade, Bread, Milk
+    }
+    enum Sexs
+    {
+        Male, Female, Helicopter
+    }
+    struct Customer
+    {
+        public Customer(string name, Sexs sex, int cockLenght)
+        {
+            Name = name;
+            Sex = sex;
+            CockLenght = cockLenght;
+        }
+        public string Name { get; }
+        public Sexs Sex { get; }
+        public int CockLenght { get; }
+    }
+    class Product
+    {
+        public Product(string name, Categorys category, float price)
+        {
+            Name = name;
+            Category = category;
+            Price = price;
+        }
+        [JsonProperty]
+        private int balls = 5;
+        public string Name { get; }
+        public Categorys Category { get; }
+        private float price;
+        public float Price
+        {
+            get
+            {
+                return price;
+            }
+
+            set
+            {
+                if (value > 0)
+                    price = value;
+            }
+        }
+    }
+    class Order
+    {
+        public Customer customer;
+        public Product product;
+        public int Number;
+        public float FullPrice => Number * product.Price;
+    }
+}
